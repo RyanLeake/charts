a653e037be36ef488663c544e662429a64ad6875
Ryan Leake <leakey94@gmail.com>
2022-02-19 18:07:47 +0000

    Revert "Update chart base protocol"
    This reverts commit e3161d468c18901d160210090adc0cd404414fd3.

--- Sources/SwiftUICharts/Base/Chart/ChartBase.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartBase.swift
@@ -3,5 +3,4 @@ import SwiftUI
 /// Protocol for any type of chart, to get access to underlying data
 public protocol ChartBase {
     var chartData: ChartData { get }
-    var comparisonChartData: ChartData { get }
 }
