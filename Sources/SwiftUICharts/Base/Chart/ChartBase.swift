e3161d468c18901d160210090adc0cd404414fd3
Ryan Leake <leakey94@gmail.com>
2022-02-19 18:06:28 +0000

    Update chart base protocol

--- Sources/SwiftUICharts/Base/Chart/ChartBase.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartBase.swift
@@ -3,4 +3,5 @@ import SwiftUI
 /// Protocol for any type of chart, to get access to underlying data
 public protocol ChartBase {
     var chartData: ChartData { get }
+    var comparisonChartData: ChartData { get }
 }
